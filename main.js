552ce3ec791db151a61959628e92c342d2f66ac3
Leandro Regueira <leandro.hregueira@gmail.com>
2022-08-27 15:41:37 -0300

    1 clase

---
+++ main.js
@@ -0,0 +1,16 @@
+for(var i =0; i <=10; i++ ) {
+ console.log("repetindo for:" + i)
+}
+
+var c = 1
+ while (c < 6){
+console.log('Hola buen día!')
+c++
+}
+
+let num = 0;
+do {
+    console.log ('Gracias!');
+    num++;
+}
+while ( num < 10)
