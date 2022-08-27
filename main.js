50fe3e535db7a54812e3953780f3cc578de78646
Leandro Regueira <leandro.hregueira@gmail.com>
2022-08-27 15:51:08 -0300

    primera clase

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
+/* Ciclo FOR */
 for(var i =0; i <=10; i++ ) {
  console.log("repetindo for:" + i)
 }
 
+/* Ciclo While*/ 
 var c = 1
  while (c < 6){
 console.log('Hola buen día!')
 c++
 }
 
+/* Ciclo DO WHILE*/ 
 let num = 0;
 do {
     console.log ('Gracias!');
